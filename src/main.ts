0809eaa721f435a867f9181223d77834c8c819cd
Stephen Valdinger <stephen@chocolatey.io>
2020-01-22 11:47:58 -0800

    found it in index.js

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
   try {
     await exec.exec(
       /*`choco install vswhere -y`*/
-      choco install vswhere -y
+      `choco install vswhere -y`
     )
   } catch (error) {
     core.setFailed(error.message)
