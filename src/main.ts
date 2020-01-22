0ea33c58bb6628cc51ab4ee60ad3d713919ede7d
Stephen Valdinger <stephen@chocolatey.io>
2020-01-22 11:41:11 -0800

    test

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function run(): Promise<void> {
+  try {
+    await exec.exec(
+      /*`choco install vswhere -y`*/
+      choco install vswhere -y
+    )
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
